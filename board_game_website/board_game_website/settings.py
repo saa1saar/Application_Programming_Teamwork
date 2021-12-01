dfaebe0a30927e414ab3b6c848fc16df871ebee6
Salli Saarinen <salli.saarinen@edu.turkuamk.fi>
2021-12-01 11:41:32 +0200

    1.1 commit_Salli

--- board_game_website/board_game_website/settings.py
+++ board_game_website/board_game_website/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#&m!dsbax!uq=104th506e-nb-0nfc#uh==%p&-bal%m5db5j4'
+SECRET_KEY = 'django-insecure-)$fs7z8!(2@=fn629#)j4pk-j(+$3$u(o3+192zmfyn&bhvnna'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
