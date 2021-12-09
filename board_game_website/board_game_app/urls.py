a84114e43437af4672118ea43e5877f8a3944f6c
ThomasOmura <90837354+ThomasOmura@users.noreply.github.com>
2021-12-09 14:46:11 +0200

    Added Page that shows all the boardgames

--- board_game_website/board_game_app/urls.py
+++ board_game_website/board_game_app/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from . import views
 app_name = 'board_game_app'
 urlpatterns = [
+    #Homepage
     path('', views.index, name='index'),
+    #Page that shows all the Board Games
+    path('BoardGames/', views.BoardGames, name='Boardgames'),
 ]
