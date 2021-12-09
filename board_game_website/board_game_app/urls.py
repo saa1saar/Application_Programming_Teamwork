40a1a781fc3df9608e029952da15c2356b97259c
SirBearington <karl.bjorn@edu.turkuamk.fi>
2021-12-09 14:33:52 +0200

    Templates and index

---
+++ board_game_website/board_game_app/urls.py
@@ -0,0 +1,8 @@
+#Defines URL patterns for board_game_app
+
+from django.urls import path
+from . import views
+app_name = 'board_game_app'
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- board_game_website/board_game_app/views.py
+++ board_game_website/board_game_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def index(request):
+    #Home page
+    return render(request, 'board_game_app/index.html')
 # Create your views here.
