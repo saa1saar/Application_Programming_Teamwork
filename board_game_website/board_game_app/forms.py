22734f23dbdaa7a995766471b50723883c81843c
SirBearington <karl.bjorn@edu.turkuamk.fi>
2021-12-10 13:14:54 +0200

    ReviewGame added to forms.py

--- board_game_website/board_game_app/forms.py
+++ board_game_website/board_game_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from .models import BoardGame
+from .models import ReviewGame
 
 class BoardGameForm(forms.ModelForm):
       
@@ -9,4 +10,9 @@ class BoardGameForm(forms.ModelForm):
             fields = ['text']
             labels = {'text': ''} 
 
-
+class ReviewGame(forms.ModelForm):
+    class Meta:
+        model = ReviewGame
+        fields = ['text']
+        labels = {'text': 'ReviewGame'}
+        widgets = {'text': forms.Textarea(attrs={'cols':80})}
