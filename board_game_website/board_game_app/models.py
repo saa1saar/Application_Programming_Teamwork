3b9bc99676487ec863b36e8ca5fd3949b771349d
ThomasOmura <90837354+ThomasOmura@users.noreply.github.com>
2021-12-11 12:28:40 +0200

    models.py edited

--- board_game_website/board_game_app/models.py
+++ board_game_website/board_game_app/models.py
@@ -2,7 +2,7 @@ from django.db import models # models tell Django how to work with data that is 
 
 # Create your models here.
 
-class BoardGame(models.Model):
+class BG(models.Model):
     """Board games that can be lent to others."""
     text = models.CharField(max_length=200) #Name of the board game
     date_added = models.DateTimeField(auto_now_add=True) #When the board game has been added to the database.
@@ -11,14 +11,15 @@ class BoardGame(models.Model):
         """Returns a strin representation of model."""
         return self.text #return the text as a string
 
-class AvailableGames(models.Model):
+class Entry(models.Model):
     """Currently avaiable board games."""
-    boardgame = models.ForeignKey(BoardGame, on_delete=models.CASCADE)
-    
+
+    bg = models.ForeignKey(BG, on_delete=models.CASCADE)
+    text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = 'avaible games'
+        verbose_name_plural = 'entries'
     
     def __str__(self):
         return f"{self.text[:50]}..."
