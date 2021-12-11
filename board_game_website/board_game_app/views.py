65e7a4c1e2425a9a88c242b2840fe2d6c6e24322
saa1saar <salli.saarinen@edu.turkuamk.fi>
2021-12-11 11:08:41 +0200

    Revert "up to page 526"
    This reverts commit 67b4d1d6e73066550e3b937ff3309473579b1920.

--- board_game_website/board_game_app/views.py
+++ board_game_website/board_game_app/views.py
@@ -16,23 +16,7 @@ def boardgame(request, boardgame_id):
     #boardgame = topic
     boardgame = BoardGame.objects.get(id=boardgame_id)
     #availablegames = entries
-    AvailableGames = boardgame.entry_set.order_by('-date_added')
-    context = {'boardgame': boardgame,'available games': AvailableGames}
+    availablegames = boardgame.entry_set.order_by('-date_added')
+    context = {'boardgame': boardgame,'available games': availablegames}
     return render(request, 'board_game_app/BoardGames.html', context)
-
-def new_boardgame(request):
-    #used for adding new games
-    if request.method != 'POST':
-        #incase there is no data -> create blank
-        form = BoardGameForm()
-    else:
-        # create a new entry with the data
-        form = BoardGameForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('board_game_app:BoardGames')
-
-    context = {'form': form}
-    return render(request, 'board_game_app/new_boardgame.html', context)
-
 # Create your views here.
