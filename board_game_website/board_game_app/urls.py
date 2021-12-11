9f581f5539ac1bfccb7fc11c35a3dc8de5f66603
saa1saar <salli.saarinen@edu.turkuamk.fi>
2021-12-11 11:09:46 +0200

    Revert "Merge branch 'main' of https://github.com/saa1saar/Application_Programming_Teamwork"
    This reverts commit 20754f99bee43683331b7a2fc6764544cde37cce, reversing
    changes made to 6f23aa9177d28b1d620e295880bce6e0d4a56ae5.

--- board_game_website/board_game_app/urls.py
+++ board_game_website/board_game_app/urls.py
@@ -6,10 +6,6 @@ app_name = 'board_game_app'
 urlpatterns = [
     #Homepage
     path('', views.index, name='index'),
-    #Page that shows all the board games
+    #Page that shows all the Board Games
     path('BoardGames/', views.BoardGames, name='Boardgames'),
-    #Detail page for a single board game 
-    path('BoardGames/<int:boardgame_id>/', views.boardgame, name='boardgame'),
-    #Page for adding a new board game
-    path('new_boardgame/', views.new_boardgame, name='new_boardgame'),
 ]
--- board_game_website/board_game_app/views.py
+++ board_game_website/board_game_app/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render
 
-from .models import BoardGame
+from .models import Topic
 
-def BoardGames(request):
-    #BoardGames
-    BoardGames = BoardGame.objects.order_by('date_added')
-    context = {'BoardGames': BoardGames}
+def index(request):
+    #Topics
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
     #Home page
-    return render(request, 'board_game_app/BoardGame.html', context)
-
-def boardgame(request, boardgame_id)
-    #boardgame = topic
-    boardgame = BoardGame.objects.get(id=topic_id)
-    #available games = entries
-    available games = topic.entry_set.order_by('-date_added')
-    context = {'boardgame': boardgame,'available games': available games}
-    return render(request, 'board_game_app/BoardGames.html', context)
+    return render(request, 'board_game_app/index.html')
 # Create your views here.
