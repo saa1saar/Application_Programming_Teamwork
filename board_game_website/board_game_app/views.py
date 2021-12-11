bd64e7661542c0139ce6f60abe9ee2bae6bb85ce
saa1saar <salli.saarinen@edu.turkuamk.fi>
2021-12-11 11:09:29 +0200

    Revert "Merge branch 'main' of https://github.com/saa1saar/Application_Programming_Teamwork"
    This reverts commit 2f5d0bccb814dccc07cd7208a03dcec1a45a95da, reversing
    changes made to 99f9c39d1425b13a7f3e98284b8be1ad004b7be2.

--- board_game_website/board_game_app/views.py
+++ board_game_website/board_game_app/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 from .models import BoardGame
-from .forms import BoardGameForm
 
 def index(request):
     """The home page for board_game_website."""
@@ -22,21 +21,4 @@ def boardgame(request, boardgame_id):
     availablegames = boardgame.entry_set.order_by('-date_added')
     context = {'boardgame': boardgame,'available games': availablegames}
     return render(request, 'board_game_app/BoardGames.html', context)
-
-def new_boardgame(request): #Page523
-    """Add a new boardgame."""
-    if request.method !='POST':
-        #No data submitted; create a blank form.
-        form = BoardGameForm()
-    else:
-        # POST data submitted; process data.
-        form = BoardGameForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('board_game_app:BoardGames')
-
-    # Display a blank or invalid form.
-    context = {'form': form}
-    returrn render(request,'board_game_app/new_boardgame.html', context)
-
 # Create your views here.
