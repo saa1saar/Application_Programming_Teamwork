5b7df6b7359e9ec0ffa8fab75f3ce58fb3974d6b
saa1saar <salli.saarinen@edu.turkuamk.fi>
2021-12-11 11:13:48 +0200

    Update views.py

--- board_game_website/board_game_app/views.py
+++ board_game_website/board_game_app/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 
 from .models import Topic
 
+<<<<<<< HEAD
 def index(request):
     #Topics
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     #Home page
     return render(request, 'board_game_app/index.html')
+=======
+def BoarGames(request):
+    #BoardGames
+    BoardGames = BoardGame.objects.order_by('date_added')
+    context = {'BoardGames': BoardGames}
+    #Home page
+    return render(request, 'board_game_app/BoardGame.html', context)
+>>>>>>> parent of 5c4dd30 (views.py Boardgame)
 # Create your views here.
